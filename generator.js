78fbb8722cb83b38c25e9638571017f65c9902a8
prateekrajgautam <prateekrajgautam@gmail.com>
2023-02-07 10:06:56 +0530

    update loadscss

--- generator.js
+++ generator.js
@@ -464,6 +464,28 @@ function GeneratorJs() {
 
 
     self.loadscss = (scss, styleid) => {
+        var path = styleid
+
+        for (i = styleid.length; i > 0; i--) {
+            if (styleid[i] == '/') {
+                path = styleid.substring(0, i)
+                break
+            }
+        }
+
+
+        // var singleLineCommentsPattern = /\/\/([^\n]*)\n/gmi
+        var importPattern = /^@([\w]*)[^"|']["|'](.*[^"|'])["|']/gmi
+        var importFilenames = scss.matchAll(importPattern)
+        var importFilenameList = Array.from(importFilenames)
+        //LoadIncludes
+        importFilenameList.forEach(n => {
+            var fileImportUrl = n[2].replaceAll('./', '');
+            var fullPath = `${path}/${fileImportUrl}.scss`
+            scss = scss.replaceAll(n[0], '')
+
+            load(fullPath)
+        })
 
         try {
 
