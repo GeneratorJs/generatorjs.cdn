059231a72a0a58256f740c737b53c7bb228abb7c
prateekrajgautam <prateekrajgautam@gmail.com>
2023-12-07 23:38:23 +0530

    bold italic not starting or ending with spaces

--- generator.js
+++ generator.js
@@ -1055,8 +1055,8 @@ function GeneratorJs() {
 
 
                 // // bold/italic/emph
-                // https://regex101.com/r/QQJ4i1/1
-                var italicPattern = /(?<=\W+)((\*|_){1,3})([^\*_\n]+?)\1(?=\W)*/gmi
+                // https://regex101.com/r/3GWtGB/1
+                var italicPattern = /(?<=\W+)((\*|_){1,3})(\S[^\*_\n]+?\S)\1(?=\W)/gmi
                 // var italicPattern = /([\s]+)((\*|_){1,3})([^\*_\n]+?)\1(?=\W)/gmi
                 match1 = md.matchAll(italicPattern)
                 matchList = Array.from(match1)
